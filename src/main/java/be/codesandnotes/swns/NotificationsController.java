f92b9bff8c3d27307450d83b0989181edaf34e9c
diego <diego@mis-tower>
2020-03-15 10:39:37 +0100

    Clean up some classes

--- src/main/java/be/codesandnotes/swns/NotificationsController.java
+++ src/main/java/be/codesandnotes/swns/NotificationsController.java
@@ -1,18 +1,12 @@
 package be.codesandnotes.swns;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Controller
 public class NotificationsController {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(NotificationsController.class);
 
     private final NotificationDispatcher dispatcher;
 
